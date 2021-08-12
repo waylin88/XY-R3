27fe60d5e8788b9e059f4a2807e1b066bff8db3a
waylin88 <86485336+waylin88@users.noreply.github.com>
2021-08-13 00:18:05 +0800

    Update and rename trunk/configs/boards/OYE-001/board.h to trunk/configs/boards/XY-R3/board.h

--- trunk/configs/boards/XY-R3/board.h
+++ trunk/configs/boards/XY-R3/board.h
@@ -1,23 +1,23 @@
-/* OYE-001 */
+/* XY-R3 */
 
 #define BOARD_PID		"XY-R3"
 #define BOARD_NAME		"XY-R3"
 #define BOARD_DESC		"XY-R3 Wireless Router"
 #define BOARD_VENDOR_NAME	"OYE"
-#define BOARD_VENDOR_URL	"http://www.oyewifi.com"
-#define BOARD_MODEL_URL		"http://www.oyewifi.com"
+#define BOARD_VENDOR_URL	"http://www.y-lin.wang"
+#define BOARD_MODEL_URL		"http://www.y-lin.wang"
 #define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
 #undef BOARD_GPIO_BTN_RESET
 #define BOARD_GPIO_BTN_WPS	13
 #undef  BOARD_GPIO_LED_ALL
-#define BOARD_GPIO_LED_WIFI	50
-#define BOARD_GPIO_LED_POWER	9
-#define BOARD_GPIO_LED_LAN	11
+#define BOARD_GPIO_LED_WIFI	7
+#define BOARD_GPIO_LED_POWER	14
+#undef BOARD_GPIO_LED_LAN
 #define BOARD_GPIO_LED_WAN	12
 #undef BOARD_GPIO_LED_USB
 #undef BOARD_GPIO_LED_ROUTER
-#undef BOARD_GPIO_PWR_USB
+#define BOARD_GPIO_PWR_USB	11
 #define BOARD_HAS_5G_11AC	0
 #define BOARD_NUM_ANT_5G_TX	0
 #define BOARD_NUM_ANT_5G_RX	0
