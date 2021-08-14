cd9c9888cc1db5402626547b22013417fe24c656
waylin88 <86485336+waylin88@users.noreply.github.com>
2021-08-14 22:30:34 +0800

    Update state.js

--- trunk/user/www/n56u_ribbon_fixed/state.js
+++ trunk/user/www/n56u_ribbon_fixed/state.js
@@ -590,7 +590,7 @@ if (found_app_wyy()){
 
 //Level 1 Menu in Gateway, Router mode
 menuL1_title = new Array("", "<#menu1#>", "", "<#menu2#>", "<#menu6#>", "<#menu4#>", "<#menu5_8#>", "<#menu5#>");
-menuL1_link = new Array("", "index.asp", "", "vpnsrv.asp", "vpncli.asp", "Main_TrafficMonitor_realtime.asp", "Advanced_System_Info.asp", "as.asp");
+menuL1_link = new Array("", "index.asp", "", "", "", "Main_TrafficMonitor_realtime.asp", "Advanced_System_Info.asp", "as.asp");
 menuL1_icon = new Array("", "icon-home", "icon-hdd", "icon-retweet", "icon-globe", "icon-tasks", "icon-random", "icon-wrench");
 
 function show_menu(L1, L2, L3){
